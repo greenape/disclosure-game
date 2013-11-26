a59150165ff8047c74372fce6a477210549e59ee
jono_work <j.gray@soton.ac.uk>
2013-11-26 16:38:35 +0000

    Probabilistic carrying capacity
    Carrying game now adds new women based on drwwing from a distribution
    rather than strict 1 for 1. Also added a caseloaded variant.

--- python/CarryingGame.py
+++ python/CarryingGame.py
@@ -2,10 +2,35 @@ import Model
 from Model import random_expectations
 import random
 from ReferralGame import *
+import collections
 
 class CarryingGame(Model.Game):
     def __unicode__(self):
         return "carrying_%s" % super(CarryingGame, self).__unicode__()
+
+    def random_player(self, probabilities, player_fn):
+        """
+        Generate a random player, based on the probabilities
+        provided by roulette.
+        """
+        draw = random.random()
+        bracket = 0.
+        for i in range(len(probabilities)):
+            bracket += probabilities[i]
+            if draw < bracket:
+                player = player_fn(player_type=i)
+                return player
+
+
+    def get_distribution(self, players):
+        """
+        Return a list giving the distribution of player types.
+        """
+        player_types = map(lambda x: x.player_type, players)
+        counts = collections.Counter(player_types)
+        return map(lambda x: x / sum(counts.values()), counts.values())
+
+
 
     """
     A game type that maintains the size of the population of
@@ -13,6 +38,7 @@ class CarryingGame(Model.Game):
     """
     def play_game(self, players):
         women, midwives = players
+        player_dist = self.get_distribution(women)
 
         rounds = self.rounds
         birthed = []
@@ -33,7 +59,7 @@ class CarryingGame(Model.Game):
                     birthed.append(woman)
                     woman.is_finished = True
                     # Add a new naive women back into the mix
-                    new_woman = type(woman)(player_type=woman.player_type)
+                    new_woman = self.random_player(player_dist, type(woman))#type(woman)(player_type=woman.player_type)
                     new_woman.init_payoffs(self.woman_baby_payoff, self.woman_social_payoff,
                         random_expectations(), [random_expectations(breadth=2) for x in range(3)])
                     new_woman.started = i
@@ -53,7 +79,73 @@ class CarryingGame(Model.Game):
             mw_res = self.measures_midwives.dump(midwives, self.rounds, self)
         return women_res, mw_res, (birthed, midwives, self)
 
+class CaseloadCarryingGame(CarryingGame):
+    def play_game(self, players):
+        women, midwives = players
+        player_dist = self.get_distribution(women)
+
+        rounds = self.rounds
+        birthed = []
+        random.shuffle(women)
+        num_midwives = len(midwives)
+        if not self.measures_women.take_at_end:
+            women_res = self.measures_women.dump(women, self.rounds, self)
+        if not self.measures_midwives.take_at_end:
+            mw_res = self.measures_midwives.dump(midwives, self.rounds, self)
+
+        caseloads = {}
+        num_women = len(women)
+        num_midwives = len(midwives)
+        load = num_women / num_midwives
+        random.shuffle(women)
+        for midwife in midwives:
+            caseloads[midwife] = []
+            for i in range(load):
+                caseloads[midwife].append(women.pop())
+
+        # Assign leftovers at random
+        while len(women) > 0:
+            caseloads[random.choice(midwives)].append(women.pop())
+
+        for i in range(rounds):
+            players = [caseloads[midwife].pop() for midwife in midwives]
+            map(self.play_round, players, midwives)
+            for x in midwives:
+                x.finished += 1
+            for i in range(len(players)):
+                woman = players[i]
+                women = caseloads[midwives[i]]
+                if self.all_played([woman], 12):
+                    birthed.append(woman)
+                    woman.is_finished = True
+                    # Add a new naive women back into the mix
+                    new_woman = self.random_player(player_dist, type(woman))#type(woman)(player_type=woman.player_type)
+                    new_woman.init_payoffs(self.woman_baby_payoff, self.woman_social_payoff,
+                        random_expectations(), [random_expectations(breadth=2) for x in range(3)])
+                    new_woman.started = i
+                    new_woman.finished = i
+                    women.insert(0, new_woman)
+                else:
+                    women.insert(0, woman)
+                    woman.finished += 1
+            if not self.measures_women.take_at_end:
+                women_res.add_results(self.measures_women.dump(players, self.rounds, self))
+            if not self.measures_midwives.take_at_end:
+                mw_res.add_results(self.measures_midwives.dump(midwives, self.rounds, self))
+        birthed += women
+        if self.measures_women.take_at_end:
+            women_res = self.measures_women.dump(birthed, self.rounds, self)
+        if self.measures_midwives.take_at_end:
+            mw_res = self.measures_midwives.dump(midwives, self.rounds, self)
+        return women_res, mw_res, (birthed, midwives, self)
+
+
 class CarryingReferralGame(CarryingGame, ReferralGame):
         """
         Just like the referral game, but maintains a carrying capacity.
         """
+
+class CarryingCaseloadReferralGame(CaseloadCarryingGame, ReferralGame)
+    """
+    Ditto, but caseloaded.
+    """
