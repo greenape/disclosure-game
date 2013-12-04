6f1c1b9b585e9b7b6b0978384f77fd54cc5b5577
jono_work <j.gray@soton.ac.uk>
2013-12-04 12:45:55 +0000

    Time

--- bash/women_proportions.sh
+++ bash/women_proportions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l walltime=01:30:00
-#PBS -l nodes=10:ppn=16
+#PBS -l nodes=1:ppn=16
 GAME[1]='-g CarryingReferralGame CarryingGame -s BayesianPayoffSignaller -r BayesianPayoffResponder'
 
 GAME[2]='-g CarryingReferralGame CarryingGame -s BayesianPayoffSignaller -r BayesianPayoffResponder -n'
@@ -25,8 +25,12 @@ GAME[11]='-g CarryingReferralGame CarryingGame -s PayoffProspectSignaller -r Rec
 
 GAME[12]='-g CarryingReferralGame CarryingGame -s PayoffProspectSignaller -r RecognitionPayoffProspectResponder -n'
 
+GAME[13]='-g CarryingReferralGame CarryingGame -s BayesianSignaller -r BayesianResponder'
+
+GAME[14]='-g CarryingReferralGame CarryingGame -s BayesianSignaller -r BayesianResponder'
+
 cd disclosure-game/python
 ulimit -n 512
 module load python
-#source /home/jg1g12/hpc/bin/activate
-python -m scoop -v HPCExperiments.py -R 10 ${GAME[$PBS_ARRAYID]}  --pickled-arguments small_test.args -f ${PBS_ARRAYID}_women_proportions_ --individual-measures -i 1000 -d ${HOME}/results
+source /home/jg1g12/hpc/bin/activate
+pypy -m scoop -v HPCExperiments.py -R 10 ${GAME[$PBS_ARRAYID]}  --pickled-arguments women_proportions.args -f ${PBS_ARRAYID}_women_proportions_ --individual-measures -i 1000 -d ${HOME}/results
