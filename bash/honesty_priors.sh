178c696620e85710bcdf5b3f64b883046e8219b0
Jono <j.gray@soton.ac.uk>
2013-12-05 15:44:28 +0000

    Priors

--- bash/honesty_priors.sh
+++ bash/honesty_priors.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-#PBS -l walltime=60:00:00
-#PBS -l nodes=12:ppn=16
+#PBS -l walltime=12:00:00
+#PBS -l nodes=1:ppn=16
+#PBS -l mem=16gb
 GAME[1]='-g CarryingReferralGame CarryingGame -s BayesianPayoffSignaller -r BayesianPayoffResponder'
 
-GAME[2]='-g CarryingReferralGame CarryingGame -s BayesianPayoffSignaller -r BayesianPayoffResponder -n'
+GAME[2]='-g CarryingReferralGame CarryingGame -s LexicographicSignaller -r LexicographicResponder'
 
-GAME[3]='-g CarryingReferralGame CarryingGame -s LexicographicSignaller -r LexicographicResponder'
+GAME[3]='-g CarryingReferralGame CarryingGame -s LexicographicSignaller -r RecognitionLexicographicResponder -n'
 
-GAME[4]='-g CarryingReferralGame CarryingGame -s LexicographicSignaller -r LexicographicResponder -n'
+GAME[4]='-g CarryingReferralGame CarryingGame -s BayesianPayoffSignaller -r RecognitionBayesianPayoffResponder -n'
 
-GAME[5]='-g CarryingReferralGame CarryingGame -s LexicographicSignaller -r RecognitionLexicographicResponder'
+GAME[5]='-g CarryingReferralGame CarryingGame -s PayoffProspectSignaller -r PayoffProspectResponder'
 
-GAME[6]='-g CarryingReferralGame CarryingGame -s LexicographicSignaller -r RecognitionLexicographicResponder -n'
+GAME[6]='-g CarryingReferralGame CarryingGame -s PayoffProspectSignaller -r RecognitionPayoffProspectResponder -n'
 
-GAME[7]='-g CarryingReferralGame CarryingGame -s BayesianPayoffSignaller -r RecognitionBayesianPayoffResponder'
+GAME[7]='-g CarryingReferralGame CarryingGame -s BayesianSignaller -r BayesianResponder'
 
-GAME[8]='-g CarryingReferralGame CarryingGame -s BayesianPayoffSignaller -r RecognitionBayesianPayoffResponder -n'
+GAME[8]='-g CarryingReferralGame CarryingGame -s BayesianSignaller -r RecognitionResponder -n'
 
 cd disclosure-game/python
 module load python
 ulimit -n 512
-python -m scoop HPCExperiments.py -R 10 ${GAME[$PBS_ARRAYID]} --pickled-arguments honesty_priors.args -f ${PBS_ARRAYID}_honesty_priors_ --individual-measures -i 1000
+python -m scoop HPCExperiments.py -R 100 ${GAME[$PBS_ARRAYID]} --pickled-arguments priors.args -f ${PBS_ARRAYID}_honesty_priors --individual-measures -i 1000
