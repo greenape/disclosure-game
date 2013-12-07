38432faca23c79c7e737f56c78b9127f177a52dc
Jono <j.gray@soton.ac.uk>
2013-12-07 22:15:46 +0000

    Tweaking run script for more runs, less things

--- bash/women_proportions.sh
+++ bash/women_proportions.sh
@@ -1,37 +1,15 @@
 #!/bin/bash
 #PBS -l walltime=12:00:00
 #PBS -l nodes=1:ppn=16
-#PBS -l mem=16gb
-GAME[1]='-g CarryingReferralGame CarryingGame -s BayesianPayoffSignaller -r BayesianPayoffResponder'
+#PBS -l pmem=1gb
+GAME[1]='-g CarryingInformationGame CaseloadSharingGame -s SharingBayesianPayoffSignaller -r SharingBayesianPayoffResponder'
 
-GAME[2]='-g CarryingReferralGame CarryingGame -s BayesianPayoffSignaller -r BayesianPayoffResponder -n'
+GAME[2]='-g CarryingInformationGame CaseloadSharingGame -s SharingLexicographicSignaller -r SharingLexicographicResponder'
 
-GAME[3]='-g CarryingReferralGame CarryingGame -s LexicographicSignaller -r LexicographicResponder'
-
-GAME[4]='-g CarryingReferralGame CarryingGame -s LexicographicSignaller -r LexicographicResponder -n'
-
-GAME[5]='-g CarryingReferralGame CarryingGame -s LexicographicSignaller -r RecognitionLexicographicResponder'
-
-GAME[6]='-g CarryingReferralGame CarryingGame -s LexicographicSignaller -r RecognitionLexicographicResponder -n'
-
-GAME[7]='-g CarryingReferralGame CarryingGame -s BayesianPayoffSignaller -r RecognitionBayesianPayoffResponder'
-
-GAME[8]='-g CarryingReferralGame CarryingGame -s BayesianPayoffSignaller -r RecognitionBayesianPayoffResponder -n'
-
-GAME[9]='-g CarryingReferralGame CarryingGame -s PayoffProspectSignaller -r PayoffProspectResponder'
-
-GAME[10]='-g CarryingReferralGame CarryingGame -s PayoffProspectSignaller -r PayoffProspectResponder -n'
-
-GAME[11]='-g CarryingReferralGame CarryingGame -s PayoffProspectSignaller -r RecognitionPayoffProspectResponder'
-
-GAME[12]='-g CarryingReferralGame CarryingGame -s PayoffProspectSignaller -r RecognitionPayoffProspectResponder -n'
-
-GAME[13]='-g CarryingReferralGame CarryingGame -s BayesianSignaller -r BayesianResponder'
-
-GAME[14]='-g CarryingReferralGame CarryingGame -s BayesianSignaller -r RecognitionResponder -n'
+GAME[3]='-g CarryingInformationGame CaseloadSharingGame -s SharingPayoffProspectSignaller -r SharingPayoffProspectResponder'
 
 cd disclosure-game/python
 ulimit -n 512
 module load python
 #source /home/jg1g12/hpc/bin/activate
-python -m scoop -v HPCExperiments.py -R 5 ${GAME[$PBS_ARRAYID]}  --pickled-arguments women_proportions.args -f ${PBS_ARRAYID}_women_proportions_ --individual-measures -i 1000 -d /scratch/jg1g12
+python -m scoop -v HPCExperiments.py -R 15 ${GAME[$PBS_ARRAYID]}  --pickled-arguments women_proportions.args -f ${PBS_ARRAYID}_women_proportions --individual-measures -i 1000 -d /scratch/jg1g12
