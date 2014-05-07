04b59c977f8113f92dc408cb7e63ccea50b9a32d
jono_work <j.gray@soton.ac.uk>
2014-05-07 16:57:19 +0100

    Caseload sensitivity

--- bash/sensitivity_jobs.sh
+++ bash/sensitivity_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-qsub -t 0-25 -vpython=/home/jg1g12/pypy/bin/pypy,sig=SharingBayesianPayoffSignaller,game=CarryingInformationGame,resp=SharingBayesianPayoffResponder sensitivity.sh
-#qsub -t 0-25 -vpython=/home/jg1g12/pypy/bin/pypy,sig=SharingPayoffProspectSignaller,game=CarryingInformationGame,resp=SharingPayoffProspectResponder w_sharing_new.sh
-qsub -t 0-25 -vpython=/home/jg1g12/pypy/bin/pypy,sig=SharingLexicographicSignaller,game=CarryingInformationGame,resp=SharingLexicographicResponder sensitivity.sh
-qsub -t 0-25 -vpython=/home/jg1g12/pypy/bin/pypy,sig=SharingProspectSignaller,game=CarryingInformationGame,resp=SharingProspectResponder sensitivity.sh
-qsub -t 0-25 -vpython=/home/jg1g12/pypy/bin/pypy,sig=SharingSignaller,game=CarryingInformationGame,resp=SharingResponder sensitivity.sh
+qsub -t 0-25 -vpython=/home/jg1g12/pypy/bin/pypy,sig=SharingBayesianPayoffSignaller,game=CaseloadSharingGame,resp=SharingBayesianPayoffResponder sensitivity.sh
+#qsub -t 0-25 -vpython=/home/jg1g12/pypy/bin/pypy,sig=SharingPayoffProspectSignaller,game=CaseloadSharingGame,resp=SharingPayoffProspectResponder w_sharing_new.sh
+qsub -t 0-25 -vpython=/home/jg1g12/pypy/bin/pypy,sig=SharingLexicographicSignaller,game=CaseloadSharingGame,resp=SharingLexicographicResponder sensitivity.sh
+qsub -t 0-25 -vpython=/home/jg1g12/pypy/bin/pypy,sig=SharingProspectSignaller,game=CaseloadSharingGame,resp=SharingProspectResponder sensitivity.sh
+qsub -t 0-25 -vpython=/home/jg1g12/pypy/bin/pypy,sig=SharingSignaller,game=CaseloadSharingGame,resp=SharingResponder sensitivity.sh
